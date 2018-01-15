e9e2aa98f2b3fc95e4edc6976dde6901381a708a
Till Domke <tilldomke@gmail.com>
2018-01-15 17:35:25 +0100

    final

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,18 @@
 class Customer < ApplicationRecord
   has_many :orders
+    
+    
+    
+    
+    def compute_all_orders
+        total_price =0;
+        orders.each do |order|
+            total_price+=order.compute_total
+        end
+        
+        return total_price
+        
+    end
+    
+    
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,4 +3,18 @@ class Order < ApplicationRecord
   has_many :line_items
   has_many :pumpkins, through: :line_items
   # has_many(:pumpkins, {through: :line_items})
+    
+    
+    
+    def compute_total
+        price=0
+        line_items.each do |line_item|
+            price+=line_item.pumpkin.price*line_item.amount
+        end
+        return price
+             
+        
+        
+    end
+    
 end
